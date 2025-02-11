9f0872fa136aa388c579396bbd0765a0c300a74d
Kauê Buriti <kaue.buriti@a8c.com>
2025-02-11 13:55:54 -0300

    chore: make it run locally

--- config.js
+++ config.js
@@ -4,28 +4,31 @@
  */
 
 const config = {
-    general: {
-        // You can find more information about the general setup in our docs: https://docs.native.express/setup/introduction
-        appName: 'Native Express', // TODO: replace with your app name
-        owner: "your-expo-account-username", // TODO replace with owner (optional) 
-        slug: 'native-express', // TODO: replace with your app slug
-        splashScreen: './src/assets/images/logo.png',
-        icon: './src/assets/images/icon.png',
-        scheme: 'nativeexpress', // TODO: replace with your app scheme
-        iosBundleIdentifier: 'com.robinfaraj.nativeexpress', // TODO: Enter your iOS bundle identifier
-        androidPackageName: 'com.robinfaraj.nativeexpress', // TODO: Enter your Android package name
-        easProjectId: 'c3746970-9ea0-4a59-8860-c60b982261c9', // TODO: Enter your EAS project ID
-    },
-    googleOauth: {
-        // More information about Google OAuth in our docs: https://docs.native.express/authentication/overview
-        iosClientId: '437910011657-ltbp98hduh1su0lcfc7ce53a9o8u08t5.apps.googleusercontent.com', // TODO: replace with your Google OAuth iOS client ID
-        iosUrlScheme: 'com.googleusercontent.apps.437910011657-ltbp98hduh1su0lcfc7ce53a9o8u08t5', // TODO: replace with your Google OAuth iOS URL scheme
-        webClientId: '437910011657-uk384cpo99kv75nsv5ar4oc3g3navibk.apps.googleusercontent.com', // TODO: replace with your Google OAuth web client ID
-    },
-    profilePage: {
-        supportPage: 'https://x.com/robin_faraj',
-        contactPage: 'https://x.com/robin_faraj',
-    },   
+  general: {
+    // You can find more information about the general setup in our docs: https://docs.native.express/setup/introduction
+    appName: '#NoSpendChallenge', // TODO: replace with your app name
+    owner: 'kaueburiti', // TODO replace with owner (optional)
+    slug: 'nospendchallenge', // TODO: replace with your app slug
+    splashScreen: './src/assets/images/logo.png',
+    icon: './src/assets/images/icon.png',
+    scheme: 'nospendchallenge', // TODO: replace with your app scheme
+    iosBundleIdentifier: 'com.company.nospendchallenge', // TODO: Enter your iOS bundle identifier
+    androidPackageName: 'com.company.nospendchallenge', // TODO: Enter your Android package name
+    easProjectId: '6ad4b39b-00b5-41de-88c5-599dcfe97089', // TODO: Enter your EAS project ID
+  },
+  googleOauth: {
+    // More information about Google OAuth in our docs: https://docs.native.express/authentication/overview
+    iosClientId:
+      '437910011657-ltbp98hduh1su0lcfc7ce53a9o8u08t5.apps.googleusercontent.com', // TODO: replace with your Google OAuth iOS client ID
+    iosUrlScheme:
+      'com.googleusercontent.apps.437910011657-ltbp98hduh1su0lcfc7ce53a9o8u08t5', // TODO: replace with your Google OAuth iOS URL scheme
+    webClientId:
+      '437910011657-uk384cpo99kv75nsv5ar4oc3g3navibk.apps.googleusercontent.com', // TODO: replace with your Google OAuth web client ID
+  },
+  profilePage: {
+    supportPage: 'https://x.com/robin_faraj',
+    contactPage: 'https://x.com/robin_faraj',
+  },
 };
 
 module.exports = config;
--- src/app/(public)/welcome.tsx
+++ src/app/(public)/welcome.tsx
@@ -11,30 +11,41 @@ import type { ImageSourcePropType } from 'react-native';
 const Welcome = () => {
   return (
     <GuestLayout>
-      <VStack className="py-12 px-4 flex-1 justify-between items-center">
+      <VStack className="flex-1 items-center justify-between px-4 py-12">
         <Center className="mb-8 mt-32">
-            <Image
-              size="md"
-              source={require('../../assets/images/logo.png') as ImageSourcePropType}
-              alt="image"
-              className="mb-8 padding-4"
-            />
-          <Heading className="text-3xl text-center">
+          <Image
+            size="md"
+            source={
+              require('../../assets/images/logo.png') as ImageSourcePropType
+            }
+            alt="image"
+            className="padding-4 mb-8"
+          />
+          <Heading className="text-center text-3xl">
             Welcome to NativeExpress!
           </Heading>
         </Center>
 
         <VStack space="md" className="w-full max-w-sm">
+          <Button
+            size="lg"
+            onPress={() => {
+              router.push('/sign-in');
+            }}
+            variant="solid"
+            className="w-full flex-1">
+            <ButtonText>Sign In</ButtonText>
+          </Button>
 
-            <Button size="lg" onPress={() => {router.push('/sign-in')}} variant="solid" className="flex-1 w-full">
-              <ButtonText>Sign In</ButtonText>
-            </Button>
-
-
-            <Button size="lg" onPress={() => {router.push('/sign-up')}} variant="solid" className="flex-1 w-full">
-              <ButtonText>Sign Up</ButtonText>
-            </Button>
-
+          <Button
+            size="lg"
+            onPress={() => {
+              router.push('/sign-up');
+            }}
+            variant="solid"
+            className="w-full flex-1">
+            <ButtonText>Sign Up</ButtonText>
+          </Button>
         </VStack>
       </VStack>
     </GuestLayout>
