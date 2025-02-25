d2ffb816d64afde3d34ff165389028ca3cb24788
Kauê Buriti <kaue.buriti@a8c.com>
2025-02-25 09:43:51 -0300

    [ui] apply colors to challenges empty component

--- src/components/home/challenges/empty.tsx
+++ src/components/home/challenges/empty.tsx
@@ -1,21 +1,19 @@
-import { Box, Button } from '@/components/ui';
+import { Box, Button, ButtonText } from '@/components/ui';
 import { Heading } from '@/components/ui/heading';
 import { Text } from '@/components/ui/text';
 import { router } from 'expo-router';
 
 const ChallengesEmptyState = () => {
   return (
-    <Box className="mb-10 flex flex-1 flex-col items-center justify-center gap-4 rounded-lg bg-slate-200 p-10">
+    <Box className="flex flex-1 flex-col items-center justify-center gap-6 rounded-lg border border-primary-500 p-10">
       <Box className="flex flex-col items-center justify-center">
         <Heading size="xl">No challenges yet</Heading>
         <Text className="text-content-50 text-center">
           Create your first challenge to get started
         </Text>
       </Box>
-      <Button
-        size="sm"
-        onPress={() => router.push('/(protected)/create-challenge')}>
-        <Text className="text-sm text-white">Create Challenge</Text>
+      <Button onPress={() => router.push('/(protected)/create-challenge')}>
+        <ButtonText>Create Challenge</ButtonText>
       </Button>
     </Box>
   );
