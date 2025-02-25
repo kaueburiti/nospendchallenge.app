ef730a67780a77c5ea43f04a2c941fd35048be05
Kauê Buriti <kaue.buriti@a8c.com>
2025-02-25 09:31:00 -0300

    [refactor] remove GoogleAuth and AppleAuth from SignUp

--- src/app/(public)/sign-up.tsx
+++ src/app/(public)/sign-up.tsx
@@ -2,30 +2,14 @@ import React from 'react';
 import { Box, VStack } from '@/components/ui';
 import { Text } from '@/components/ui';
 import GuestLayout from '../../components/GuestLayout';
-import { Image } from '@/components/ui';
 import { Center } from '@/components/ui';
 import { Heading } from '@/components/ui';
 import { Link as ExpoLink } from 'expo-router';
-import { useSignInWithGoogle } from '@/hooks/auth/useSignInWithGoogle';
-import { useSignInWithApple } from '@/hooks/auth/useSignInWithApple';
-import { type ImageSourcePropType } from 'react-native';
 import SignUpForm from '../../components/sign-up/SignUpForm';
 import { LinkText } from '@/components/ui';
-import AuthButton from '../../components/auth/AuthButton';
 import MeditatingWoman from '@/components/ui/illustrations/meditating-woman';
 
 const SignUp = () => {
-  const { signInWithGoogle } = useSignInWithGoogle();
-  const { signInWithApple } = useSignInWithApple();
-
-  const handleGoogleSignIn = async () => {
-    await signInWithGoogle({});
-  };
-
-  const handleAppleSignIn = async () => {
-    await signInWithApple({});
-  };
-
   return (
     <GuestLayout>
       <VStack className={'flex-1 px-4 py-12'}>
@@ -40,15 +24,6 @@ const SignUp = () => {
         </Box>
 
         <SignUpForm />
-        {/* <Text className={'text-center font-bold my-4'}>or</Text>
-        <AuthButton
-          provider="Google"
-          onPress={handleGoogleSignIn}
-        />
-        <AuthButton
-          provider="Apple"
-          onPress={handleAppleSignIn}
-        /> */}
         <Text className={'mt-8 text-center'}>
           Already have an account?{' '}
           <ExpoLink href="/sign-in">
