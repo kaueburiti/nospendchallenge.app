6a838ac42ef28581f01fba990e9f42e6d0f403d1
Kauê Buriti <kaue.buriti@a8c.com>
2025-03-04 10:02:50 -0300

    [challenges]: reflect the check ins into the days grid

--- src/components/home/challenges/days-grid.tsx
+++ src/components/home/challenges/days-grid.tsx
@@ -2,42 +2,62 @@ import React from 'react';
 import { Box, Text } from '@/components/ui';
 import { useLocalSearchParams } from 'expo-router';
 import { useChallenge } from '@/hooks/challenges';
-import { differenceInDays } from 'date-fns';
+import { addDays, differenceInDays, format } from 'date-fns';
 import classNames from 'classnames';
-
+import { useGetUserChecksByChallenge } from '@/hooks/checks';
 export default function DaysGrid() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const { data: challenge, isLoading } = useChallenge(id);
+  const { data: checks } = useGetUserChecksByChallenge(Number(id));
 
-  if (isLoading) {
+  if (isLoading || !challenge) {
     return null;
   }
 
-  const numberOfDays = challenge
-    ? differenceInDays(
-        new Date(challenge.end_date),
-        new Date(challenge.start_date),
-      )
-    : 0;
+  const allDaysBetweenDates = (startDate: string, endDate: string) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const days = [];
+    const totalDays = differenceInDays(end, start) + 1;
+
+    for (let i = 0; i < totalDays; i++) {
+      const date = addDays(start, i);
+      days.push(format(date, 'yyyy-MM-dd'));
+    }
+    return days;
+  };
+
+  const days = allDaysBetweenDates(challenge?.start_date, challenge?.end_date);
 
   return (
     <Box className="mx-auto flex w-[336px] flex-row flex-wrap justify-start gap-2">
-      {Array.from({ length: numberOfDays }, (_, index) => index + 1).map(
-        day => (
-          <Day key={day} day={day} />
-        ),
-      )}
+      {days.map((day, index) => (
+        <Day
+          key={day}
+          dayPosition={index}
+          isChecked={checks?.some(check => check.date === day) ?? false}
+        />
+      ))}
     </Box>
   );
 }
 
-const Day = ({ day }: { day: number }) => {
+const Day = ({
+  isChecked,
+  dayPosition,
+}: {
+  dayPosition: number;
+  isChecked: boolean;
+}) => {
   return (
     <Box
       className={classNames(
         'h-12 w-12 items-center justify-center rounded-md bg-gray-300',
+        {
+          'bg-success-500': isChecked,
+        },
       )}>
-      <Text className="text-md font-bold text-white">{day}</Text>
+      <Text className="text-md font-bold text-white">{dayPosition + 1}</Text>
     </Box>
   );
 };
