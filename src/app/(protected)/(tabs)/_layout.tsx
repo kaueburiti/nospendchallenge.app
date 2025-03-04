59cc43173477e785880801738b3d8437eeeb22a1
Kauê Buriti <kaue.buriti@a8c.com>
2025-03-04 13:37:16 -0300

    feat: create the challenges tab

--- src/app/(protected)/(tabs)/_layout.tsx
+++ src/app/(protected)/(tabs)/_layout.tsx
@@ -2,10 +2,9 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { HomeIcon, User } from 'lucide-react-native';
-import {useTheme} from "@/hooks/useTheme";
+import { HomeIcon, TrophyIcon, User } from 'lucide-react-native';
+import { useTheme } from '@/hooks/useTheme';
 import { i18n } from '@/i18n';
-
 
 export default function TabLayout() {
   const { isDark } = useTheme();
@@ -20,16 +19,25 @@ export default function TabLayout() {
       <Tabs.Screen
         name="home"
         options={{
-          title: i18n.t("home.tab_label"),
+          title: i18n.t('home.tab_label'),
           tabBarIcon: ({ color }) => (
             <TabBarIcon icon={HomeIcon} color={color} />
           ),
         }}
       />
       <Tabs.Screen
+        name="challenges"
+        options={{
+          title: 'Challenges',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon icon={TrophyIcon} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="profile"
         options={{
-          title: i18n.t("profile.tab_label"),
+          title: i18n.t('profile.tab_label'),
           tabBarIcon: ({ color }) => <TabBarIcon icon={User} color={color} />,
         }}
       />
---
+++ src/app/(protected)/(tabs)/challenges.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import { Box, Heading, VStack } from '@/components/ui';
+import { SafeAreaView } from '@/components/ui/SafeAreaView';
+import { i18n } from '@/i18n';
+import { Section } from '@/components/Section';
+import { PlusCircle } from 'lucide-react-native';
+import { router } from 'expo-router';
+import { Button } from '@/components/ui';
+import ChallengeList from '@/components/home/challenges';
+
+const ChallengesPage = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView className="h-[1px] flex-1">
+        <Section>
+          <VStack space="4xl">
+            <Box className="flex flex-1 flex-col overflow-auto">
+              <Box className="mb-6 flex flex-row items-center justify-between">
+                <Heading size="3xl">Challenges</Heading>
+                <Button
+                  onPress={() => router.push('/(protected)/create-challenge')}>
+                  <PlusCircle size={24} color="white" />
+                </Button>
+              </Box>
+              <ChallengeList />
+            </Box>
+          </VStack>
+        </Section>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default ChallengesPage;
