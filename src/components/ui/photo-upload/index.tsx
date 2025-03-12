25475f6b1b4c06520c1540f32024b323c01c5002
Kauê Buriti <kaue.buriti@a8c.com>
2025-03-12 13:03:25 -0300

    [avatar]: update the photo avatar as soon you choose the photo

--- src/components/ui/photo-upload/index.tsx
+++ src/components/ui/photo-upload/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { Box, Image } from '@/components/ui';
+import React, { useState, useEffect } from 'react';
+import { Avatar, AvatarFallbackText, AvatarImage } from '@/components/ui';
 import * as ImagePicker from 'expo-image-picker';
 import { Pressable, Alert } from 'react-native';
-import { ImagePlus } from 'lucide-react-native';
 
 interface ImageData {
   uri: string;
@@ -13,15 +12,26 @@ interface ImageData {
 interface PhotoUploadProps {
   onImageUpload: (imageData: ImageData) => void;
   uri?: string;
+  fallbackText?: string;
 }
 
-export default function PhotoUpload({ onImageUpload, uri }: PhotoUploadProps) {
+export default function PhotoUpload({
+  onImageUpload,
+  uri,
+  fallbackText,
+}: PhotoUploadProps) {
+  const [imageUri, setImageUri] = useState<string | undefined>(uri);
+
+  useEffect(() => {
+    setImageUri(uri);
+  }, [uri]);
+
   const pickImage = async () => {
     try {
       const result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         allowsEditing: true,
-        aspect: [16, 9],
+        aspect: [1, 1],
         quality: 1,
         base64: true,
       });
@@ -30,6 +40,7 @@ export default function PhotoUpload({ onImageUpload, uri }: PhotoUploadProps) {
         const file = result.assets[0];
         if (file.base64) {
           const fileExt = file.uri.split('.').pop()?.toLowerCase() ?? 'png';
+          setImageUri(file.uri);
           onImageUpload({
             uri: file.uri,
             base64: file.base64,
@@ -45,17 +56,10 @@ export default function PhotoUpload({ onImageUpload, uri }: PhotoUploadProps) {
 
   return (
     <Pressable onPress={pickImage} className="items-center">
-      <Box className="h-64 w-64 items-center justify-center overflow-hidden rounded-full border-[12px] border-primary-500 bg-primary-0">
-        {uri ? (
-          <Image
-            source={{ uri }}
-            className="h-full w-full"
-            alt="Challenge cover"
-          />
-        ) : (
-          <ImagePlus size={64} color="white" />
-        )}
-      </Box>
+      <Avatar size="5xl" className="border-2 border-white">
+        <AvatarFallbackText>{fallbackText}</AvatarFallbackText>
+        {imageUri && <AvatarImage source={{ uri: imageUri }} />}
+      </Avatar>
     </Pressable>
   );
 }
