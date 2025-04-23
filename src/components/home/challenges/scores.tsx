2c4ed16bc33c4cd36f5455f0a90b8fd7280bb57b
Kauê Buriti <kaue.buriti@a8c.com>
2025-04-23 15:57:49 -0300

    [challenge]: fix the days calculations

--- src/components/home/challenges/scores.tsx
+++ src/components/home/challenges/scores.tsx
@@ -3,7 +3,12 @@ import { Box, Divider, Heading, Text, VStack } from '@/components/ui';
 import { useLocalSearchParams } from 'expo-router';
 import { useChallenge } from '@/hooks/challenges';
 import { useGetUserChecksByChallenge } from '@/hooks/checks';
-import { differenceInDays, eachDayOfInterval, format } from 'date-fns';
+import {
+  differenceInDays,
+  eachDayOfInterval,
+  isBefore,
+  isSameDay,
+} from 'date-fns';
 import { useTranslation } from '@/hooks/useTranslation';
 
 export default function ChallengeScores() {
@@ -20,9 +25,10 @@ export default function ChallengeScores() {
   const startDate = new Date(challenge.start_date);
   const endDate = new Date(challenge.end_date);
   const today = new Date();
+  today.setHours(0, 0, 0, 0);
 
   // Calculate days left
-  const daysLeft = Math.max(0, differenceInDays(endDate, today) + 2);
+  const daysLeft = Math.max(0, differenceInDays(endDate, today) + 1);
 
   // Calculate total checks
   const totalChecks = checks?.length ?? 0;
@@ -31,25 +37,23 @@ export default function ChallengeScores() {
   const allDays = eachDayOfInterval({
     start: startDate,
     end: today < endDate ? today : endDate,
-  }).map(date => format(date, 'yyyy-MM-dd'));
-
-  // Get all checked days
-  const checkedDays = new Set(
-    checks?.map(check => format(new Date(check.date), 'yyyy-MM-dd')) ?? [],
-  );
+  }).map(date => date);
 
   // Calculate skipped days (days that have passed but have no check)
   const daysSkipped = allDays.filter(day => {
-    const todayStr = format(today, 'yyyy-MM-dd');
-    const sameDay = day === todayStr;
+    const sameDay = isSameDay(day, today);
 
-    return !checkedDays.has(day) && !sameDay;
+    return (
+      !sameDay && !checks?.some(check => isSameDay(new Date(check.date), day))
+    );
   }).length;
+
+  const challengeNotStarted = isBefore(today, startDate);
 
   return (
     <Box className="my-5 flex-row">
       <VStack className="flex-1 items-center border-r border-outline-300 py-2">
-        <Heading size="4xl">{daysLeft}</Heading>
+        <Heading size="4xl">{challengeNotStarted ? '-' : daysLeft}</Heading>
         <Text size="xs">{t('checks.days_left')}</Text>
       </VStack>
       <Divider
@@ -57,7 +61,7 @@ export default function ChallengeScores() {
         className="flex w-1 self-center bg-background-300"
       />
       <VStack className="flex-1 items-center border-r border-outline-300 py-2">
-        <Heading size="4xl">{totalChecks}</Heading>
+        <Heading size="4xl">{challengeNotStarted ? '-' : totalChecks}</Heading>
         <Text size="xs">{t('checks.checks')}</Text>
       </VStack>
       <Divider
@@ -65,7 +69,7 @@ export default function ChallengeScores() {
         className="flex w-1 self-center bg-background-300 sm:hidden"
       />
       <VStack className="flex-1 items-center pt-2">
-        <Heading size="4xl">{daysSkipped}</Heading>
+        <Heading size="4xl">{challengeNotStarted ? '-' : daysSkipped}</Heading>
         <Text size="xs">{t('checks.days_skipped')}</Text>
       </VStack>
     </Box>
