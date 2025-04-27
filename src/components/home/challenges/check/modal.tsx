ecbb84542ff2aff81b2df57b0e1d634fac5ac517
Kauê Buriti <kaue.buriti@a8c.com>
2025-04-27 14:21:55 -0300

    [check]: replace check modal with botto drawer

--- src/components/home/challenges/check/modal.tsx
+++ src/components/home/challenges/check/modal.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Heading, VStack, Text } from '@/components/ui';
-import { ModalBody } from '@/components/ui/modal';
-import { CloseIcon, Icon } from '@/components/ui/icon';
-import { ModalCloseButton, Modal } from '@/components/ui/modal';
-import { ModalHeader } from '@/components/ui/modal';
-import { ModalContent } from '@/components/ui/modal';
-import { ModalBackdrop } from '@/components/ui/modal';
 import { CheckInForm } from '@/components/home/challenges/check/form';
 import { useTranslation } from '@/hooks/useTranslation';
+import { BottomDrawer } from '@/components/BottomDrawer';
+import { KeyboardAvoidingView } from '@/components/ui/keyboard-avoiding-view';
+
 type CheckModalProps = {
   isOpen: boolean;
   onClose: () => void;
@@ -22,32 +19,20 @@ export default function CheckModal({
   const { t } = useTranslation();
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="md">
-      <ModalBackdrop />
-      <ModalContent>
-        <ModalHeader className="mb-2 flex items-start">
-          <VStack space="xs">
-            <Heading size="md" className="text-typography-950">
-              {t('checks.form.title')}
-            </Heading>
-            <Text>{t('checks.form.description')}</Text>
-          </VStack>
-          <ModalCloseButton>
-            <Icon
-              as={CloseIcon}
-              size="md"
-              className="stroke-background-400 group-[:active]/modal-close-button:stroke-background-900 group-[:focus-visible]/modal-close-button:stroke-background-900 group-[:hover]/modal-close-button:stroke-background-700"
-            />
-          </ModalCloseButton>
-        </ModalHeader>
-        <ModalBody>
+    <KeyboardAvoidingView behavior="padding">
+      <BottomDrawer
+        isOpen={isOpen}
+        onClose={onClose}
+        title={t('checks.form.title')}>
+        <VStack space="md" className="w-full">
+          <Text>{t('checks.form.description')}</Text>
           <CheckInForm
             challengeId={challengeId}
             onSubmit={onClose}
             onClose={onClose}
           />
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+        </VStack>
+      </BottomDrawer>
+    </KeyboardAvoidingView>
   );
 }
