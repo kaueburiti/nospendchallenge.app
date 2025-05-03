955abc986ec08958a2314624943a1f4c57de161c
Kauê Buriti <kaue.buriti@a8c.com>
2025-05-03 13:17:50 -0300

    [notifications]: remove OneSignal request permission to start using in app notification

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -75,13 +75,8 @@ function RootLayout() {
 
   // Initialize OneSignal in useEffect to ensure it runs only once
   useEffect(() => {
-    // Enable verbose logging for debugging (remove in production)
-    OneSignal.Debug.setLogLevel(LogLevel.Verbose);
     // Initialize with your OneSignal App ID
     OneSignal.initialize(process.env.EXPO_PUBLIC_ONE_SIGNAL_APP_ID!);
-    // Use this method to prompt for push notifications.
-    // We recommend removing this method after testing and instead use In-App Messages to prompt for notification permission.
-    void OneSignal.Notifications.requestPermission(false);
   }, []); // Ensure this only runs once on app mount
 
   if (!loaded || !isReady) {
