f8dcac393a334f2f7f73fd41ad010d25ae30a9f8
Kauê Buriti <kaue.buriti@a8c.com>
2025-05-14 18:47:35 -0300

    [ui]: improve profile card in home

--- src/components/profile/card.tsx
+++ src/components/profile/card.tsx
@@ -6,6 +6,7 @@ import { Badge } from '@/components/ui/badge';
 import { type User as SupabaseUser } from '@supabase/supabase-js';
 import { useProfile } from '@/hooks/useProfile';
 import { router } from 'expo-router';
+import { AlertCircle } from 'lucide-react-native';
 
 interface ProfileCardProps {
   user: SupabaseUser | null;
@@ -23,11 +24,12 @@ export const ProfileCard: React.FC<ProfileCardProps> = () => {
       <Pressable onPress={() => router.push('/profile')} className="relative">
         {hasToUpdateProfile && (
           <Badge
-            className="absolute bottom-0 left-0 z-10 h-4 w-4 rounded-full bg-error-500 text-center"
-            variant="solid"
-          />
+            className="absolute -bottom-1 -left-1 z-10 flex h-5 w-5 items-center justify-center rounded-full bg-primary-500 p-0 text-center"
+            variant="solid">
+            <AlertCircle size={16} color="white" />
+          </Badge>
         )}
-        <Avatar className="bg-primary-500">
+        <Avatar className="border-2 border-primary-500 bg-primary-500">
           <AvatarFallbackText>{profile?.display_name}</AvatarFallbackText>
           {profile?.avatar_url && (
             <AvatarImage
